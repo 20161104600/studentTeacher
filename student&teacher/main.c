c4f08ec02ba85db2dba72ebe23f9bca294a7e843
20161104600 <a20161104600@mac15.local>
2018-06-25 10:21:29 +0800

    Initial Commit

---
+++ student&teacher/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  student&teacher
+//
+//  Created by 20161104600 on 18/6/25.
+//  Copyright © 2018年 20161104600. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
